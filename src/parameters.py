1be280f53830681981d776aa1d9a5777d9031939
ruben <87551697+rbnnn@users.noreply.github.com>
2023-02-13 19:49:12 -0500

    uploaded script for news scraping & json export
    parser is not operational yet
    search will be edited

---
+++ src/parameters.py
@@ -0,0 +1,70 @@
+# commonly used parameters
+
+lang = ['af', #0 Afrikaans
+        'ar', #1 Arabic
+        'bg', #2 Bulgarian
+        'bn', #3 Bengali
+        'ca', #4 Catalan
+        'cs', #5 Czech
+        'cy', #6 Welsh
+        'cn', #7
+        'da', #8 Danish
+        'de', #9
+        'el', #10
+        'en', #11 English
+        'es', #12 Espanol
+        'et', #13
+        'fa', #14
+        'fi', #15
+        'fr', #16 French
+        'gu', #17
+        'he', #18
+        'hi', #19
+        'hr', #20
+        'hu', #21
+        'id', #22
+        'it', #23
+        'ja', #24
+        'kn', #25
+        'ko', #26
+        'lt', #27
+        'lv', #28
+        'mk', #29
+        'ml', #30
+        'mr', #31
+        'ne', #32
+        'nl', #33
+        'no', #34
+        'pa', #35
+        'pl', #36
+        'pt', #37
+        'ro', #38
+        'ru', #39
+        'sk', #40
+        'sl', #41
+        'so', #42
+        'sq', #43
+        'sv', #44
+        'sw', #45
+        'ta', #46
+        'te', #47
+        'th', #48
+        'tl', #49
+        'tr', #50
+        'tw', #51
+        'uk', #52
+        'ur', #53
+        'vi'  #54
+        ]
+
+topic = [
+        'news', 'sport', 'tech',                    #0, 1, 2
+        'world', 'finance','politics',              #3, 4, 5
+        'business', 'economics', 'entertainment',   #6, 7, 8
+        'beauty', 'travel', 'music',                #9, 10, 11
+        'food', 'science', 'gaming',                #12, 13, 14
+        'energy']                                   #15
+
+countries = []
+
+sources = []
